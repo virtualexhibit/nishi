291f47a10a34c6bd3419e9bf109727b6d3f50838
virtualexhibit <virtualexhibit22@gmail.com>
2025-09-22 21:11:36 +0800

    dropdown & textarea

---
+++ SelectInput.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="mb-3">
+    <label class="input-label">{{ item.display.text }}</label>
+    <select class="form-select" v-bind="item.directives">
+      <option
+        v-for="(option, idx) in item.options"
+        :key="idx"
+        :value="option.value"
+      >
+        {{ option.text }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SelectInput",
+  props: {
+    item: { type: Object, required: true }
+  }
+}
+</script>
+
+
+<!-- in the recipe -->
+<!-- {
+  component: "SelectInput",
+  directives: { name: "pet_type", class: "form-select" },
+  display: { text: "Pet Type:" },
+  options: [
+    { value: "dog", text: "Dog" },
+    { value: "cat", text: "Cat" },
+    { value: "other", text: "Other" }
+  ]
+}, -->
---
+++ TextAreaInput.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="mb-3">
+    <label class="input-label">{{ item.display.text }}</label>
+    <textarea
+      class="form-control"
+      :rows="item.directives.rows || 3"
+      v-bind="item.directives"
+    ></textarea>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "TextAreaInput",
+  props: {
+    item: { type: Object, required: true },
+  },
+};
+</script>
+
+
+<!-- in recipe
+
+   {
+    component: "TextAreaInput",
+    directives: { id: "specialNotes", class: "form-control", rows: 3, placeholder: "Write notes here..." },
+    display: { text: "Special Notes:" },
+  }, -->
